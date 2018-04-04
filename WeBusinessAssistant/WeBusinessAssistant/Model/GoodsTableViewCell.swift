eefd4b3a246f5cd158bf2244ab0d5f54addb984a
Zealot817 <234632198@qq.com>
2018-04-04 17:18:03 +0800

    201804041715
    change controller fron UIViewController to UITableViewController
    learn how to use UITableViewController class
    now can add a cell to TableView

---
+++ WeBusinessAssistant/WeBusinessAssistant/Model/GoodsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  GoodsTableViewCell.swift
+//  WeBusinessAssistant
+//
+//  Created by 刘启 on 2018/4/4.
+//  Copyright © 2018年 刘启. All rights reserved.
+//
+
+import UIKit
+
+class Goods : NSObject {
+    
+}
+
+class GoodsTableViewCell: UITableViewCell {
+    static let identifier = "GoodsCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
